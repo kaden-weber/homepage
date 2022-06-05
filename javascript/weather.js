3116a8e51e61782d19cb4bbdc71560e612e95422
Kaden Weber <kaden.weber@showpad.com>
2022-06-04 23:40:26 -0500

    add weather to the clock display; removing bootstrap etc.

--- javascript/weather.js
+++ javascript/weather.js
@@ -1,18 +1,26 @@
 (() => {
-	const init = () => {
-		updateWeather();
-		setInterval(updateWeather, 3600000);
-	};
+  const init = () => {
+    checkWeather();
+    setInterval(checkWeather, 3600000);
+  };
 
-    function updateWeather() {
-        fetch('https://wttr.in/?format=j1')
-        .then(response => response.json())
-        .then(data => displayWeather(data));
-    }
-	
-    function displayWeather(data) {
-		document.getElementsByClassName('current-temp')[0].innerHTML = data.current_condition[0].temp_F
-    }
+  function checkWeather() {
+    fetch("https://wttr.in/?format=j1")
+      .then((response) => response.json())
+      .then((data) => displayWeather(data));
+  }
 
-	return init();
-})()
+  function displayWeather(data) {
+    const currentConditions = data.current_condition[0];
+
+    document.getElementById("current-temp").innerHTML =
+      currentConditions.temp_F;
+    document.getElementById("location").innerHTML =
+      data.nearest_area[0].areaName[0].value;
+
+    document.getElementById("weather-description").innerHTML =
+      currentConditions.weatherDesc[0].value;
+  }
+
+  return init();
+})();
